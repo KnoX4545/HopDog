95923b48ab544bed72e286b3d5af188517069d02
KnoX <ahora.masuod.m9061@gmail.com>
2026-07-20 15:53:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,18 +14,15 @@ logging.basicConfig(level=logging.INFO)
 def main():
     app = Application.builder().token(TOKEN).build()
     
-    # دستورات
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
     
-    # دستورات ادمین
     app.add_handler(CommandHandler("setlevel", set_user_level))
     app.add_handler(CommandHandler("addlevel", add_user_level))
     app.add_handler(CommandHandler("setpoint", set_user_point))
     app.add_handler(CommandHandler("addpoint", add_user_point))
     app.add_handler(CommandHandler("userinfo", get_user_info))
     
-    # هندلرها
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_handler(CallbackQueryHandler(handle_callback))
     app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, group_welcome))
