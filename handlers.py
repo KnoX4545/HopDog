cd5a05d23033bd0a54b478d8b5d2c50c1cb36885
KnoX <ahora.masuod.m9061@gmail.com>
2026-07-23 20:56:11 +0330

    Delete handlers.py

--- handlers.py
+++
@@ -1,8 +0,0 @@
-# Compatibility wrapper
-from core_handlers import *
-from hapo_handler import *
-from bank_handler import *
-from profile_handler import *
-from leaderboard_handler import *
-from admin_handler import *
-from callback_handler import *
